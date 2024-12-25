8042190a9a07b25ae833b3b2da6d95e123fab71b
Ibrahemherzallah <ibrahemherzallah@gmail.com>
2024-12-25 16:36:25 +0200

    solve bug

--- src/main/java/com/example/travl/HelloApplication.java
+++ src/main/java/com/example/travl/HelloApplication.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("client-details.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("agent-dash.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setFullScreen(true);
